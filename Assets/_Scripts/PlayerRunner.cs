27395b4afbf400237fd02b350874937844f34f06
lmhoai2003 <hoailmps30600@fpt.edu.vn>
2025-07-24 16:29:38 +0700

    1234

--- Assets/_Scripts/PlayerRunner.cs
+++ Assets/_Scripts/PlayerRunner.cs
@@ -1,47 +1,61 @@
 
 using UnityEngine;
 using Fusion;
+using Unity.Collections;
 
 public class PlayerRunner : SimulationBehaviour, IPlayerJoined
 {
-    [SerializeField] private GameObject playerPrefab1;
-    [SerializeField] private GameObject playerPrefab2;
+    [SerializeField] private GameObject[] playerPrefab;
+    public int chonNhanVat;
 
-    private readonly Vector3[] spawnPoints = new Vector3[]
-    {
-        new Vector3(51, 10, 20),
-        new Vector3(60, 10, 35),
-        new Vector3(60, 10, 50)
-    };
+    public float x1, y1, z1;
+    public float x2, y2, z2;
+    public float x,y,z;
+
 
 
     public void PlayerJoined(PlayerRef player)
     {
         if (player == Runner.LocalPlayer)
+
         {
-            // Ví dụ: Lấy lựa chọn nhân vật từ PlayerPrefs (giả định đã lưu trước đó)
-            // 0 = nhân vật 1, 1 = nhân vật 2
-            int selectedCharacterIndex = PlayerPrefs.GetInt("player", 0);
-
-            GameObject selectedPrefab = selectedCharacterIndex == 1 ? playerPrefab2 : playerPrefab1;
-
-            // Chọn ngẫu nhiên một điểm spawn
-            int randomIndex = Random.Range(0, spawnPoints.Length);
-            Vector3 spawnPos = spawnPoints[randomIndex];
-
-            // Spawn nhân vật đã chọn
-            Runner.Spawn(selectedPrefab, spawnPos, Quaternion.identity, Runner.LocalPlayer, (runner, obj) =>
+            var ran = Random.Range(1, 3);
+            if (ran == 1)
             {
-                var playerSetup = obj.GetComponent<PlayerSetup>();
-                if (playerSetup != null)
-                {
-                    playerSetup.SetupCamera(); 
-                }
-            });
-
+                x = x1;
+                y = y1;  
+                z = z1;
+            }
+            else
+            {
+                x = x2;
+                y = y2;
+                z = z2;
+            }
         }
+
+
+        // Spawn nhân vật đã chọn
+        Runner.Spawn(playerPrefab[chonNhanVat], new Vector3(x,y,z), Quaternion.identity, Runner.LocalPlayer, (runner, obj) =>
+        {
+            var playerSetup = obj.GetComponent<PlayerSetup>();
+            if (playerSetup != null)
+            {
+                playerSetup.SetupCamera();
+            }
+        });
+
     }
-
-
-
+    public void ChonNhanVat1()
+    {
+        chonNhanVat = 0;
+    }
+    public void ChonNhanVat2()
+    {
+        chonNhanVat = 1;
+    }
 }
+
+
+
+
